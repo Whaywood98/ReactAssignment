60c2bd2ca362a271de5dd60eb87da67c01f74a8f
William Haywood <william.haywood981@gmail.com>
2020-12-22 18:21:50 -0500

    Trouble shooting functional components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Main from './components/MainComponent';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+    
+    render() {
+        return (
+            <div>
+                <Main />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/MenuDetailComponent.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+function RenderDish({dish}) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        } else {
+            return (<div></div>);
+        }
+}
+
+function RenderComments({comments}) {
+            if (comments != null) {
+                const dishComments = comments.map((comment) => {
+                    const date = new Date(comment.date);
+                    return (
+                        <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>{comment.author}</p>
+                            <p>{date.toDateString()}</p>
+                        </li>
+                    );
+                });
+                return (
+                    <div>
+                        <h3>Comments</h3>
+                        <div>{dishComments}</div>
+                    </div>
+                );
+                        }
+            else {
+                return (
+                    <div></div>
+                );
+            }
+}
+
+const MenuDetail = (props) => {
+    if(props.dish != null)
+    return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish}/>
+                        <RenderComments comments={props.dish.comments}/>
+                    </div>
+                </div>
+            </div>
+            );
+    }
+
+export default MenuDetail;
