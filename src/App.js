ee4497f7929634f58c172730cfaf851ccacac608
William Haywood <william.haywood981@gmail.com>
2020-12-26 09:31:34 -0500

    added router code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import Main from './components/MainComponent';
 import './App.css';
 import { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
     
     render() {
         return (
-            <div>
-                <Main />
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,17 @@
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
 import MenuDetail from './MenuDetailComponent';
 import { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES,
-            selectedDish: null
+            dishes: DISHES
         };
     }
 
@@ -19,13 +20,21 @@ class Main extends Component {
     }
 
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            )
+        }
+
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes}
-                    onClick={(dishId) => this.onDishSelect(dishId)}/>
-                <MenuDetail
-                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch> 
                 <Footer />
             </div>
         );
